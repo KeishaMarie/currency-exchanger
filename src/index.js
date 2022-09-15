824da998d2c2dd4dbffd6446c203d7b31e52e409
Keisha Hepner <kmhepner90@gmail.com>
2022-09-14 19:52:57 -0700

    Create and update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {CurrencyExchangerService} from './currency-exchanger-service.js';
 
 // Business Logic 
+function currencyExchanger() {
+  CurrencyExchangerService.currencyExchanger()
+    .then(function(response) {
+      if (response.main) {
+        printElements(response);
+      } else {
+        printError(response);
+      }
+    });
+}
