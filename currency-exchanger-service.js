0932a07e290eb3cbb4402ac4c898ac67cd0becd1
Keisha Hepner <kmhepner90@gmail.com>
2022-09-18 14:25:44 -0700

    Receiving error message Error: Can't resolve './currency-exchanger-service.js' in '/Users/peckii/Desktop/currency-exchanger/src'  @ ./src/index.js 4:0-73 8:2-26. trying to figure out why it won't connect.

--- currency-exchanger-service.js
+++ currency-exchanger-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyExchangerService {
   static currencyExchanger() {
-    return fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           return response.json()
--- src/index.js
+++ src/index.js
@@ -4,16 +4,34 @@ import './css/styles.css';
 import {CurrencyExchangerService} from './currency-exchanger-service.js';
 
 // Business Logic 
-function currencyExchanger() {
-  CurrencyExchangerService.currencyExchanger()
+function currencyExchanger(userInput, currencySelected) {
+  CurrencyExchangerService.currencyExchanger(userInput, currencySelected)
     .then(function(response) {
       if (response.conversion_rates) {
-        printElements(response);
+        printElements(response, userInput, currencySelected);
       } else {
-        printError(response);
+        printError(response, userInput, currencySelected);
       }
     });
 }
 
 
 // UI Logic
+function printElements(response, userInput, currencySelected) {
+  document.querySelector('p#output').innerText = `The exchange rate for ${userInput} USD is ${(response.conversion_rates[currencySelected] * userInput)} ${currencySelected}.`;
+}
+
+function printError(response, userInput) {
+  document.querySelector('p#output').innerText = `Unable to access currency exchange data for ${userInput} USD. ${response}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let userInput = document.querySelector('#userInput').value;
+  let currencySelected = document.querySelector('#currencySelected').value;
+  currencyExchanger(userInput, currencySelected);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form#currencyForm').addEventListener("submit", handleFormSubmission);
+});
